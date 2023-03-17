3104f2e5f419614f88de3c4081b4579d7c4e3018
Jonatan <javevang1@gmail.com>
2023-03-17 14:21:44 +0100

    Finsished header

--- Frontend/ecom/src/components/Header.vue
+++ Frontend/ecom/src/components/Header.vue
@@ -1,1 +1,90 @@
-<template><h1>hello</h1></template>
+<template>
+    <div class="header" :class="{ 'is-scrolled': scrollPosition > 125 }">
+      <div class="logo">
+        <h1>LOGO</h1>
+      </div>
+
+        <div class="links">
+          <a href="">Ny annonse</a>
+          <a href="">Meldinger</a>
+          <a href="">Logg inn</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrollPosition: 0
+    };
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  }
+};
+</script>
+
+<style scoped>
+.header {
+  position: fixed;
+  top: 0;
+  background: radial-gradient(#f7fee7,#84cc16);
+  display: flex;
+  flex-direction: row;
+  place-items: center;
+  justify-content: space-between;
+  max-height: 125px;
+  transition: opacity 0.5s ease;
+  opacity: 1;
+  width: 100%;
+  height: 100vh;
+}
+
+.header.is-scrolled {
+  opacity: 0.8;
+}
+
+.logo {
+    color: #0f172a;
+}
+
+.logo h1{
+    font-size: 60px;
+}
+
+.logo img {
+  height: 15rem;
+}
+
+.links {
+  padding-right: 5%;
+}
+
+.links a {
+  color: #0f172a;
+  text-decoration: none;
+  border-radius: 15px;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  margin-right: 25px;
+  font-size: 30px;
+}
+
+.links a:hover {
+  color: #f8fafc;
+}
+
+@media (max-width: 768px) {
+  .links a {
+    font-size: 1.2rem;
+  }
+  .logo h1{
+    font-size: 30px;
+  }
+}
+</style>
