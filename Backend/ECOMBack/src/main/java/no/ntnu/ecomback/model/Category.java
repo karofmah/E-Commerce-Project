76091fb102f2c5504049819df1a910acd8635af8
Edvard Sørby <117030279+edvardsorby@users.noreply.github.com>
2023-03-17 14:28:53 +0100

    Updated Item.java

---
+++ Backend/ECOMBack/src/main/java/no/ntnu/ecomback/model/Category.java
@@ -0,0 +1,7 @@
+package no.ntnu.ecomback.model;
+
+public enum Category {
+    ELECTRONICS,
+    VEHICLE,
+    REALESTATE
+}
--- Backend/ECOMBack/src/main/java/no/ntnu/ecomback/model/Item.java
+++ Backend/ECOMBack/src/main/java/no/ntnu/ecomback/model/Item.java
@@ -1,4 +1,86 @@
 package no.ntnu.ecomback.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "item")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "briefDescription")
+    private String briefDescription;
+
+    @Column(name = "fullDescription")
+    private String fullDescription;
+
+    @Column(name = "category")
+    private Category category;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "price")
+    private int price;
+
+    public Item() {
+
+    }
+
+    public Item(String briefDescription, String fullDescription, Category category, String location, int price) {
+        this.briefDescription = briefDescription;
+        this.fullDescription = fullDescription;
+        this.category = category;
+        this.location = location;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBriefDescription() {
+        return briefDescription;
+    }
+
+    public void setBriefDescription(String briefDescription) {
+        this.briefDescription = briefDescription;
+    }
+
+    public String getFullDescription() {
+        return fullDescription;
+    }
+
+    public void setFullDescription(String fullDescription) {
+        this.fullDescription = fullDescription;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
