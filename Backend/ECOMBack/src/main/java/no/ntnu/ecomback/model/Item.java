c86f8650816402e23ad11665a99541afe5f638d5
Edvard Sørby <117030279+edvardsorby@users.noreply.github.com>
2023-03-19 15:37:39 +0100

    Updated Item.java, added Location.java and implemented CategoryConverter.java

--- Backend/ECOMBack/src/main/java/no/ntnu/ecomback/model/Item.java
+++ Backend/ECOMBack/src/main/java/no/ntnu/ecomback/model/Item.java
@@ -2,34 +2,72 @@ package no.ntnu.ecomback.model;
 
 import jakarta.persistence.*;
 
+/**
+ * The Item class represents an item (advert) published in the e-commerce application.
+ */
 @Entity
 @Table
 public class Item {
+
+    /**
+     * The Id of the item
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    /**
+     * The seller of the item
+     */
+    @ManyToOne
+    private User seller;
+
+    /**
+     * A brief description of the item
+     */
     @Column
     private String briefDescription;
 
+    /**
+     * The full description of the item
+     */
     @Column
     private String fullDescription;
 
-    @Column
+    /**
+     * The category of item
+     */
     private Category category;
 
-    //create location class?
-    @Column
-    private String location;
+    /**
+     * The location of the item
+     */
+    @Embedded
+    private Location location;
 
+    /**
+     * The price of the item
+     */
     @Column
     private int price;
 
+    /**
+     * Instantiates a new Item.
+     */
     public Item() {
 
     }
 
-    public Item(String briefDescription, String fullDescription, Category category, String location, int price) {
+    /**
+     * Instantiates a new Item.
+     * @param briefDescription the brief description
+     * @param fullDescription  the full description
+     * @param category         the category
+     * @param location         the location
+     * @param price            the price
+     */
+    public Item(User seller, String briefDescription, String fullDescription, Category category, Location location, int price) {
+        this.seller = seller;
         this.briefDescription = briefDescription;
         this.fullDescription = fullDescription;
         this.category = category;
@@ -37,50 +75,106 @@ public class Item {
         this.price = price;
     }
 
+    /**
+     * Gets id.
+     * @return the id
+     */
     public long getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    /**
+     * Gets seller.
+     * @return the seller
+     */
+    public User getSeller() {
+        return seller;
     }
 
+    /**
+     * Sets seller
+     * @param seller the seller of the item
+     */
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    /**
+     * Gets brief description.
+     * @return the brief description
+     */
     public String getBriefDescription() {
         return briefDescription;
     }
 
+    /**
+     * Sets brief description.
+     * @param briefDescription the brief description
+     */
     public void setBriefDescription(String briefDescription) {
         this.briefDescription = briefDescription;
     }
 
+    /**
+     * Gets full description.
+     * @return the full description
+     */
     public String getFullDescription() {
         return fullDescription;
     }
 
+    /**
+     * Sets full description.
+     * @param fullDescription the full description
+     */
     public void setFullDescription(String fullDescription) {
         this.fullDescription = fullDescription;
     }
 
+    /**
+     * Gets category.
+     * @return the category
+     */
     public Category getCategory() {
         return category;
     }
 
+    /**
+     * Sets category.
+     * @param category the category
+     */
     public void setCategory(Category category) {
         this.category = category;
     }
 
-    public String getLocation() {
+    /**
+     * Gets location.
+     * @return the location
+     */
+    public Location getLocation() {
         return location;
     }
 
-    public void setLocation(String location) {
+    /**
+     * Sets location.
+     * @param location the location
+     */
+    public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     * Gets price.
+     * @return the price
+     */
     public int getPrice() {
         return price;
     }
 
+    /**
+     * Sets price.
+     * @param price the price
+     */
     public void setPrice(int price) {
         this.price = price;
     }
---
+++ Backend/ECOMBack/src/main/java/no/ntnu/ecomback/model/Location.java
@@ -0,0 +1,49 @@
+package no.ntnu.ecomback.model;
+
+import jakarta.persistence.Embeddable;
+
+/**
+ * The Location class is used to represent the exact location of an Item
+ */
+@Embeddable
+public class Location {
+    private double latitude;
+    private double longitude;
+
+    /**
+     * Gets latitude.
+     * @return the latitude
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Sets latitude.
+     * @param latitude the latitude
+     */
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * Gets longitude.
+     * @return the longitude
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * Sets longitude.
+     * @param longitude the longitude
+     */
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + latitude + ", " + longitude + ")";
+    }
+}
