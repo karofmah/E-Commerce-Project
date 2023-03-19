4fac599e41a18657a333e70d5dae26c960783dcb
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-03-19 15:36:02 +0100

    New user may be registered to database

--- Backend/ECOMBack/src/main/java/no/ntnu/ecomback/service/UserService.java
+++ Backend/ECOMBack/src/main/java/no/ntnu/ecomback/service/UserService.java
@@ -1,7 +1,43 @@
 package no.ntnu.ecomback.service;
 
+import no.ntnu.ecomback.model.User;
+import no.ntnu.ecomback.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public ResponseEntity<User> registerUser(User user) {
+        try {
+            List<User> users = new ArrayList<>(userRepository.findAll());
+
+            for (User u : users) {
+                if (Objects.equals(u.getEmail(), user.getEmail())) {
+                    System.out.println("User already exists");
+                    return new ResponseEntity<>(HttpStatus.OK);
+                }
+            }
+            System.out.println("User registered successfully");
+            User savedUser = userRepository.save(user);
+            return new ResponseEntity<>(savedUser,HttpStatus.CREATED);
+        } catch (Exception e) {
+            System.out.println("Error occurred while registering user: " + e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
