ecd45e3337265b87cb257351e72157251851e355
Edvard Sørby <117030279+edvardsorby@users.noreply.github.com>
2023-03-20 09:18:06 +0100

    Added get-endpoint in ItemController

--- Backend/ECOMBack/src/main/java/no/ntnu/ecomback/controller/ItemController.java
+++ Backend/ECOMBack/src/main/java/no/ntnu/ecomback/controller/ItemController.java
@@ -3,7 +3,12 @@ package no.ntnu.ecomback.controller;
 import no.ntnu.ecomback.model.Item;
 import no.ntnu.ecomback.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/items")
@@ -20,5 +25,16 @@ public class ItemController {
     @PostMapping("/add")
     public Item addItem(@RequestBody Item item){
         return itemService.addItem(item);
+    }
+
+    @GetMapping("/getItems")
+    public ResponseEntity<List<Item>> getItems() {
+        try {
+            List<Item> items = new ArrayList<>();
+
+            return new ResponseEntity<>(items, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
