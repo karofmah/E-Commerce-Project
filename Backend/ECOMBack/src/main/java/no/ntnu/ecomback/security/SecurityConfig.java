5664288cdccd464d65ab5f3e5107736666ae99a2
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-03-20 09:12:40 +0100

    Improved security

---
+++ Backend/ECOMBack/src/main/java/no/ntnu/ecomback/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package no.ntnu.ecomback.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class SecurityConfig {
+
+    // inject SecurityFilterChain and tell that all requests are authenticated
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().and()
+                .authorizeHttpRequests()
+                .requestMatchers("/token").permitAll()
+                .anyRequest().authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
