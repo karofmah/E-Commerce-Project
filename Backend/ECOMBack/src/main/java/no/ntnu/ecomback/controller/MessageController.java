3192662365186a84b28772746ea5faa980973784
Edvard Sørby <117030279+edvardsorby@users.noreply.github.com>
2023-03-21 16:08:04 +0100

    Added endpoints for messaging feature

---
+++ Backend/ECOMBack/src/main/java/no/ntnu/ecomback/controller/MessageController.java
@@ -0,0 +1,61 @@
+package no.ntnu.ecomback.controller;
+
+import no.ntnu.ecomback.model.Message;
+import no.ntnu.ecomback.model.User;
+import no.ntnu.ecomback.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/api/messages")
+@CrossOrigin("http://localhost:5173/")
+public class MessageController {
+
+    private MessageService messageService;
+
+    @Autowired
+    public void setMessageService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/sendMessage")
+    public Message sendMessage(@RequestBody Message message){
+        System.out.println(message);
+        return messageService.addMessage(message);
+    }
+
+
+    @GetMapping("/getAllMessages")
+    public ResponseEntity<List<Message>> getItems() {
+        try {
+            List<Message> messages = messageService.getAllMessages();
+
+            return new ResponseEntity<>(messages, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{fromEmail}/{toEmail}")
+    public List<Message> getMessagesByToEmailAndFromEmail(@PathVariable String fromEmail,
+                                                       @PathVariable String toEmail){
+        System.out.println(fromEmail);
+        System.out.println(toEmail);
+        return messageService.getMessagesInBothDirections(toEmail, fromEmail);
+    }
+
+
+    @GetMapping("/{email}/contacts")
+    public List<Optional<User>> getContacts(@PathVariable String email){
+        System.out.println(email);
+        return messageService.getContacts(email);
+    }
+
+}
