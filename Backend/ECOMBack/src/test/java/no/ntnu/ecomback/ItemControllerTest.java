6627f90c4e38e3e09325c8dc62fa6f056b459cd3
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-03-23 13:27:21 +0100

    Created test for getItems() endpoint

--- Backend/ECOMBack/src/test/java/no/ntnu/ecomback/ItemControllerTest.java
+++ Backend/ECOMBack/src/test/java/no/ntnu/ecomback/ItemControllerTest.java
@@ -1,8 +1,12 @@
 package no.ntnu.ecomback;
 
 import no.ntnu.ecomback.model.*;
+import no.ntnu.ecomback.repository.ItemRepository;
 import no.ntnu.ecomback.service.ItemService;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,6 +25,7 @@ import java.util.List;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,35 +39,45 @@ public class ItemControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private ItemService itemService;
+    private static ItemRepository itemRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
 
+     List<Item> mockItems=new ArrayList<>();
+
+    @BeforeEach
+    @DisplayName("Setting up mock data for tests")
+    public void setup() {
+        User user1=new User("karofm@ntnu.no","Karo","Mahmoud","karofm","pw",Role.NORMAL_USER);
+        Location location=new Location();
+        location.setLatitude(5);
+        location.setLongitude(10);
+        List<String> images=new ArrayList<>();
+        images.add("image1");
+        mockItems.add(new Item(user1, "Description 1", "Full description 1",new Category("Category 1"),location ,100));
+        mockItems.add(new Item(user1, "Description 2", "Full description 2",new Category("Category 2"),location ,150));
+        mockItems.add(new Item(user1, "Description 3", "Full description 3",new Category("Category 3"),location ,200));
+
+        // Set up mock behavior for ItemRepository
+        when(itemRepository.findAll()).thenReturn(mockItems);
+    }
+
     @Test
+    @DisplayName("Testing the endpoint for retrieving all items")
     public void testGetItems() throws Exception {
-/*
-        User seller1 = new User("karofm3@ntnu.no","Karo", "Mahmoud", "karofm", "pw", Role.NORMAL_USER);
-        Location location1 = new Location();
-        location1.setLatitude(45.5);
-        location1.setLatitude(50.0);
-        List<String> images1 = new ArrayList<>();
-        images1.add("image1");
-        images1.add("image2");
-        Item item1 = new Item(seller1,"item","about item", Category.REAL_ESTATE,location1,100);
-        List<Item> expectedItemList = Arrays.asList(item1);
 
         MvcResult result = mockMvc.perform(get("/api/items/getItems")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
 
-        TypeReference<List<Item>> typeReference = new TypeReference<>() {};
-        List<Item> actualItemList = objectMapper.readValue(result.getResponse().getContentAsString(), typeReference);
+        String responseString = result.getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        List<Item> actualItems = mapper.readValue(responseString, new TypeReference<>() {
+        });
 
-
-        assertEquals(expectedItemList, actualItemList);
-
-*/
+        assertEquals(mockItems.size(), actualItems.size());
     }
+    
 }
