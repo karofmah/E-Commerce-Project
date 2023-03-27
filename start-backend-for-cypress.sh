4755457f0ccea8d90009af878b8e47767bc2d73b
Jonatan André Vevang <jonatanv@stud.ntnu.no>
2023-03-27 14:53:02 +0200

    Delete start-backend-for-cypress.sh

--- start-backend-for-cypress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export SPRING_PROFILES_ACTIVE=central
-export SPRING_DATASOURCE_URL=jdbc:mysql://mysql-ait.stud.idi.ntnu.no:3306/jonatanv
-export SPRING_DATASOURCE_USERNAME=jonatanv
-export SPRING_DATASOURCE_PASSWORD=MnUUreHOz
-
-java -jar Backend/ECOMBack/target/ECOMBack-0.0.1-SNAPSHOT.jar
